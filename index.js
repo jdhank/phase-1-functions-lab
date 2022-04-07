c6afed009c9205e3db7e8219142e2ec68eeb3b56
Jake Hankins <jakehankins@comcast.net>
2022-04-07 18:01:24 -0400

    commit tree 688f9bc43971bb15cd7fe3073c056b54bb9ef31b

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+let hq=42
+
+function distanceFromHqInBlocks(distance){
+    if (distance > hq)
+    return distance - hq
+    else if (hq > distance)
+     return hq - distance
+}
+
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance)*264
+}
+function distanceTravelledInFeet(start, destination){
+    if (destination > start)
+    return (destination - start) * 264
+    else if (start > destination)
+    return (start - destination) *264
+}
+function calculatesFarePrice(start, destination){
+    const travel= distanceTravelledInFeet(start,destination)
+    if (travel < 400)
+    return 0
+    else if (travel <= 2000)
+    return (travel -400)* .02
+    else if (travel > 2000 && travel <2500)
+    return 25
+    else 
+    return 'cannot travel that far'
+}
